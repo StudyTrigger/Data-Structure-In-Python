fe9198e306e2a0a25efa0b459acb85bb26c36765
Study Trigger <132209985+StudyTrigger@users.noreply.github.com>
2026-04-08 16:39:33 +0530

    Search node

---
+++ LinkedList/Video_7_SearchNode.py
@@ -0,0 +1,58 @@
+#Search an element into Linked List 
+class Node:
+    def __init__(self,data):
+        self.info=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    
+    def insbeg(self,ele):
+        current=Node(ele)
+        if self.head is None:
+            self.head=current
+        else:
+            current.next=self.head
+            self.head=current
+    
+    def display(self):
+        if self.head is None:
+            print("Empty Linked List")
+            return
+        else:
+            current=self.head
+            while current:
+                print(current.info," -> ", end="")
+                current=current.next
+            print("None")
+    
+    def search(self,ele):
+        if self.head is None:
+            print("Empty Linked List")
+            return
+        else:
+            found=-1
+            count=1
+            current=self.head
+            while current:
+                if current.info==ele:
+                    found=count
+                    break
+                count+=1
+                current=current.next
+            if found==-1:
+                print("Element not found in linked list")
+            else:
+                print("Element found at ",found, "position")
+
+mylist=LinkedList()
+n=int(input("How many elements you want to insert"))
+for i in range(n):
+    ele=int(input("Enter element"))
+    mylist.insbeg(ele)
+
+print("My linked list")
+mylist.display()
+ele=int(input("Enter element to be search "))
+mylist.search(ele)
